cd946e03340e1ce806fb28d9c96ee7de0226d282
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2024-05-01 22:23:12 -0600

    idk

--- scripts/copy-public.ts
+++ scripts/copy-public.ts
@@ -1,9 +1,12 @@
 import fs from "fs";
 
-if (!fs.existsSync("out/spotify-widget-win32-x64/public")) {
-    fs.mkdirSync("out/spotify-widget-win32-x64/public");
+console.log(fs.readdirSync("./"));
+console.log(fs.readdirSync("./out"));
+
+if (!fs.existsSync("./out/spotify-widget-win32-x64/public")) {
+    fs.mkdirSync("./out/spotify-widget-win32-x64/public");
 }
 
-fs.copyFileSync(".webpack/x64/renderer/main_window/index.html", "out/spotify-widget-win32-x64/public/index.html");
-fs.copyFileSync(".webpack/x64/renderer/main_window/index.js", "out/spotify-widget-win32-x64/public/index.js");
-fs.copyFileSync("src/index.css", "out/spotify-widget-win32-x64/public/index.css");
+fs.copyFileSync("./.webpack/x64/renderer/main_window/index.html", "./out/spotify-widget-win32-x64/public/index.html");
+fs.copyFileSync("./.webpack/x64/renderer/main_window/index.js", "./out/spotify-widget-win32-x64/public/index.js");
+fs.copyFileSync("./src/index.css", "./out/spotify-widget-win32-x64/public/index.css");
