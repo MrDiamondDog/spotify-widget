f45e059dcd5b502653e9a5743a2f37a50384700b
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2024-05-01 22:25:30 -0600

    got it

--- scripts/copy-public.ts
+++ scripts/copy-public.ts
@@ -1,12 +1,18 @@
 import fs from "fs";
 
-console.log(fs.readdirSync("./"));
-console.log(fs.readdirSync("./out"));
-
 if (!fs.existsSync("./out/spotify-widget-win32-x64/public")) {
     fs.mkdirSync("./out/spotify-widget-win32-x64/public");
 }
 
-fs.copyFileSync("./.webpack/x64/renderer/main_window/index.html", "./out/spotify-widget-win32-x64/public/index.html");
-fs.copyFileSync("./.webpack/x64/renderer/main_window/index.js", "./out/spotify-widget-win32-x64/public/index.js");
-fs.copyFileSync("./src/index.css", "./out/spotify-widget-win32-x64/public/index.css");
+// check device
+if (process.platform === "win32") {
+    fs.copyFileSync("./.webpack/x64/renderer/main_window/index.html", "./out/spotify-widget-win32-x64/public/index.html");
+    fs.copyFileSync("./.webpack/x64/renderer/main_window/index.js", "./out/spotify-widget-win32-x64/public/index.js");
+    fs.copyFileSync("./src/index.css", "./out/spotify-widget-win32-x64/public/index.css");
+} else if (process.platform === "linux") {
+    fs.copyFileSync("./.webpack/x64/renderer/main_window/index.html", "./out/spotify-widget-linux-x64/public/index.html");
+    fs.copyFileSync("./.webpack/x64/renderer/main_window/index.js", "./out/spotify-widget-linux-x64/public/index.js");
+    fs.copyFileSync("./src/index.css", "./out/spotify-widget-linux-x64/public/index.css");
+} else {
+    console.log("unknown platform " + process.platform);
+}
